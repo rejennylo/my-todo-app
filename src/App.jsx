c1cb2b33504f9e4f997906de62214e693d3323d5
rejennylo <rejennylo@gmail.com>
2024-09-07 16:11:15 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>My Todo App</h1>
+    </>
+  )
+}
+
+export default App
